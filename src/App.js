0dd45835794c21a377c78ea1b37b30ef0ee644fe
SeherKumsar <181805036@stu.adu.edu.tr>
2024-03-04 23:27:45 +0300

    displayed list of books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
 
@@ -15,10 +16,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  return <div>
-    {books.length}
-    <BookCreate onCreate={createBook} />
-  </div>;
+  return (
+    <div className="app">
+      <BookList books={books} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
