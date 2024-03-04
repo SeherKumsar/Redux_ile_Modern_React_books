c737573634c86528c87f1b420f97268cdf7ec82b
SeherKumsar <seherkumsar99@outlook.com>
2024-03-04 11:40:42 +0300

    re-render the component with the updated books array (BAD CODE)

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with:', title);
+    // BAD CODE
+    books.push({ id: 123, title: title });
+    console.log(books);
+    setBooks(books); // 
   };
 
   return <div>
+    {books.length}
     <BookCreate onCreate={createBook} />
   </div>;
 }
