4e57d9224c4e1ea53456f3526cdcb45ae965edd1
SeherKumsar <181805036@stu.adu.edu.tr>
2024-03-05 02:11:11 +0300

    added book is in response.data object to see it in console

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
@@ -25,14 +26,20 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [ // Add a new book to the list
-      ...books, // Copy the existing books
-      { id: Math.round(Math.random() * 9999), // Generate a random ID
-        title, // Set the title
-      },
-    ];
-    setBooks(updatedBooks);
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+    
+    console.log(response);
+
+    // const updatedBooks = [ // Add a new book to the list
+    //   ...books, // Copy the existing books
+    //   { id: Math.round(Math.random() * 9999), // Generate a random ID
+    //     title, // Set the title
+    //   },
+    // ];
+    // setBooks(updatedBooks);
   };
 
   return (
