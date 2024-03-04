57b117bdd0205f942432519fd7e2459430230277
SeherKumsar99 <seher.kumsar@lotusaibilisim.com>
2024-03-04 22:26:42 +0300

    added updatedBooks funtions

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ function App() {
 
   const createBook = (title) => {
     // BAD CODE
-    books.push({ id: 123, title: title });
-    console.log(books);
-    setBooks(books); // 
+    // books.push({ id: 123, title: title }); // modifying a property of the state directly
+    // books[0] = { id: 123, title: title }; // modifying an element of the state directly
+    // books.id = 123; 
+    // console.log(books);
+    // setBooks(books);
+    const updatedBooks = [
+      ...books,
+      { id: 123, title: title }
+    ];
+    setBooks(updatedBooks);
   };
 
   return <div>
