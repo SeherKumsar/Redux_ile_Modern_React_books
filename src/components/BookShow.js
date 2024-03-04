0e004579ab0305a636a53a54169b9d29691a3a23
SeherKumsar <181805036@stu.adu.edu.tr>
2024-03-04 23:53:18 +0300

    toggling form display

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
 function BookShow ({ book , onDelete}) {
 
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false); // Default value is false (we don't want to show the edit form)
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
     };
 
+    const handleEditClick = () => {
+        setShowEdit(!showEdit); // Toggle the value of showEdit
+    };
+
+    let content = <h3>{book.title}</h3>;
+
+    if (showEdit) {
+        content = <BookEdit />;
+    }
+
     return <div className="book-show">
-        {book.title}
+        
+        <div>{content}</div>
+
         <div className="actions">
-            <button className="delete" onClick={handleClick}>
+            <button className="edit" onClick={handleEditClick}>
+                Edit
+            </button>
+            <button className="delete" onClick={handleDeleteClick}>
                 Delete
             </button>
         </div>
