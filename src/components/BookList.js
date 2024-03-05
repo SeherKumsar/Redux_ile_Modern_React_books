16a13f24541b1978672b6442f2d7d0086e54b4dc
SeherKumsar <seherkumsar99@outlook.com>
2024-03-05 22:20:56 +0300

    Props and Context Together

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,14 @@
-// import { useContext } from 'react';
-// import BooksContext from '../context/books';
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
+  const { books } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+      <BookShow key={book.id} book={book} /> 
+      // Pass the book object as a prop to BookList constructor so that we can access it in BookShow
     );
   });
 
