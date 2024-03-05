981ebe26c7a6054f2101d11b1f59f276c1093de5
SeherKumsar <seherkumsar99@outlook.com>
2024-03-05 20:56:03 +0300

    showed the built-in provider itself

--- src/context/books.js
+++ src/context/books.js
@@ -1,6 +1,26 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BooksContext = createContext();
 
+function Provider({ children }) {
+  const [count, setCount] = useState(5);
 
+  const valueToShare = {
+    count: count,
+    incrementCount: () => {
+        setCount(count + 1);
+    }
+  }
+
+  return (
+    <BooksContext.Provider value={valueToShare}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
+
+export { Provider};
 export default BooksContext;
+
+// import { Provider } from '/./a.sdf'; // only provider
+// import BooksContext  from '/./a.sdf'; // only context 
