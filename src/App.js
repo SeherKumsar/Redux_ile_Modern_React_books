959584282d0c3eb4e3aaf870ec0ecf986b6c781f
SeherKumsar <seherkumsar99@outlook.com>
2024-03-05 21:38:15 +0300

    remove useState and axios to use the new state

--- src/App.js
+++ src/App.js
@@ -1,66 +1,21 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { seEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 function App() {
 
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-    setBooks(response.data);
-  };
+  const { fetchBooks } = useContext(BooksContext);
 
   useEffect(() => { // Fetch the books when the component mounts
     fetchBooks();
   }, []);
 
-  const editBookById = async (id, newTitle) => {
-    // await axios.put('http://localhost:3001/books/' + id'
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    console.log(response.data); // Log the updated book to the console
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data}; // get the whole book object and update the title
-      }
-      return book; // Keep the book as it is
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-  await axios.delete(`http://localhost:3001/books/${id}`); // Delete the book from the server
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id; // Keep all the books that don't have the ID we want to delete (silinen id'yi hariç tut ve diğerlerini döndür)
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-    
-    const updatedBooks = [ // Add a new book to the list
-      ...books, // Copy the existing books
-      response.data, // Add the new book
-    ];
-    setBooks(updatedBooks);
-  };
-
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
