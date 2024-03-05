b707fa9957c72e6e3338d2901d59968250c1d162
SeherKumsar <seherkumsar99@outlook.com>
2024-03-05 20:18:42 +0300

    context in action

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -19,4 +19,5 @@ function BookList({ books, onDelete, onEdit }) {
   );
 }
 
+
 export default BookList;
--- src/context/books.js
+++ src/context/books.js
@@ -2,4 +2,5 @@ import { createContext } from 'react';
 
 const BooksContext = createContext();
 
+
 export default BooksContext;
